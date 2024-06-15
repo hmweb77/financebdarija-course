91175ebfd6d9db1ff9ba862f1333a0902fe9beca
hmweb77 <oussamahamdani1@gmail.com>
2024-06-15 21:48:52 +0100

    new update

--- src/actions.js
+++
@@ -1,7 +0,0 @@
-import { sessionOptions,SessionData } from "./lib"
-import {getIronSession} from "iron-session"
-export const getSession = async () => {
-    const session = await getIronSession
-}
-export const login = async () => {}
-export const logout = async () => {}
